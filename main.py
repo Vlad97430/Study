78d8bd96d17c0f3714d91886a51520387b9ec964
Vlad97430 <vladyslavkrav6@gmail.com>
2024-10-21 16:00:09 +0300

    Практична 1

---
+++ main.py
@@ -0,0 +1,8 @@
+greeting = "Hello world"
+firstname = "Влад"
+lastname = "Кравчук"
+age = 16
+print("Значення змінної greeting -", greeting, "Тип змінної -", type(greeting))
+print("Значення змінної firstname -", firstname, "Тип змінної -", type(firstname))
+print("Значення змінної lastname -", lastname,  "Тип змінної -", type(lastname))
+print("Значення змінної age -", age, "Тип змінної -", type(age))
